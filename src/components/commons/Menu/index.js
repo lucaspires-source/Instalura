b5c35bc45c686e7167cf328ac8539c8863e62727
lucaspires-source <pires.lucas94@gmail.com>
2021-07-27 17:07:57 -0300

    adicionando links e logo dinamicamente

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -1,11 +1,29 @@
 import { MenuWrapper } from "./styles/MenuWrapper";
-
+import { Logo } from "../../../theme/Logo";
 const Menu = () => {
+  const links = [
+    { texto: "Home", url: "/" },
+    { texto: "Perguntas Frequentes", url: "/faq" },
+    { texto: "Sobre", url: "/sobre" },
+  ];
   return (
     <MenuWrapper>
-      <MenuWrapper.LeftArea>Logo Area</MenuWrapper.LeftArea>
-      <MenuWrapper.CenterArea>Links Area</MenuWrapper.CenterArea>
-      <MenuWrapper.RightArea>Buttons Area</MenuWrapper.RightArea>
+      <MenuWrapper.LeftArea>
+        <Logo />
+      </MenuWrapper.LeftArea>
+      <MenuWrapper.CenterArea>
+        {links.map((link)=>{
+          return (
+            <li> 
+              <a href={link.url}>{link.texto}</a>
+            </li>
+          )
+        })}
+      </MenuWrapper.CenterArea>
+      <MenuWrapper.RightArea>
+        <button>Entrar</button>
+        <button>Cadastrar</button>
+      </MenuWrapper.RightArea>
     </MenuWrapper>
   );
 };
