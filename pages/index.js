95022171500c3a59f68f46702f1dce98fb6380ca
lucaspires-source <pires.lucas94@gmail.com>
2021-07-27 16:19:30 -0300

    nova estrutura de arquivos

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
-import styled from 'styled-components'
-
-const MenuWrapper = styled.nav `
-font-family: 'Rubik', 'sans-serif';
-display: flex;
-align-items: center;
-justify-content:space-between;
-flex-wrap: wrap;
-margin-top:18px;
-padding-left: 28px;
-padding-right:28px;
-
-`
-
-
-const Menu = () => {
-  return(
-    <MenuWrapper>
-      <div>
-          Logo Area
-      </div>
-      <div>
-        Links Area
-      </div>
-      <div>
-        Buttons Area
-      </div>
-    </MenuWrapper>
-  )
-}
+import Menu from '../src/components/commons/Menu';
 
 export default function Home() {
-  return <div><Menu /></div>;
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
 }
---
+++ src/components/commons/Menu/index.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const MenuWrapper = styled.nav`
+  font-family: "Rubik", "sans-serif";
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin-top: 18px;
+  padding-left: 28px;
+  padding-right: 28px;
+`;
+
+const Menu = () => {
+  return (
+    <MenuWrapper>
+      <div>Logo Area</div>
+      <div>Links Area</div>
+      <div>Buttons Area</div>
+    </MenuWrapper>
+  );
+};
+
+
+export default Menu;
