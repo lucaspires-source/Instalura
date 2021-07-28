ad28af258fabf03ee8fcfc6e3bce4fd8a091eff5
lucaspires-source <pires.lucas94@gmail.com>
2021-07-28 16:55:08 -0300

    adicionando o Head

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
-import { ThemeProvider } from 'styled-components'
-import theme from '../src/theme'
-import {GlobalStyle} from '../src/theme/GlobalStyle'
-
+import Head from 'next/head';
+import { ThemeProvider } from "styled-components";
+import theme from "../src/theme";
+import { GlobalStyle } from "../src/theme/GlobalStyle";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
