ee324fda59bd6e21d5c103b946fd4b42e0e38b47
lucaspires-source <pires.lucas94@gmail.com>
2021-07-28 17:00:39 -0300

    adicionando o footer

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Menu from '../src/components/commons/Menu';
-
+import Footer from '../src/components/commons/Footer';
 export default function Home() {
   return (
     <div>
       <Menu />
+      <Footer />
     </div>
   );
 }
