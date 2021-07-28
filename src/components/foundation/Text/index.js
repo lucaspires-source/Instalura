4b5a4b6b6ba3e5b1765af01654131e4d4d306785
lucaspires-source <pires.lucas94@gmail.com>
2021-07-28 19:10:59 -0300

    adicionando os demais typographyVariants para o  TextStylevariantsmap

--- src/components/foundation/Text/index.js
+++ src/components/foundation/Text/index.js
@@ -1,21 +1,48 @@
 import styled, { css } from "styled-components";
 
-export const TextStyleVartiansMap ={
-  smallestException : css `
-      font-size: ${({ theme }) => theme.typographyVariants.smallestException.fontSize};
-      font-weight: ${({ theme }) =>theme.typographyVariants.smallestException.font_weight};
-      line-height: ${({ theme }) =>theme.typographyVariants.smallestException.lineHeight};
-  
+export const TextStyleVartiansMap = {
+  title: css`
+    font-size: ${({ theme }) => theme.typographyVariants.title.fontSize};
+    font-weight: ${({ theme }) => theme.typographyVariants.title.font_weight};
+    line-height: ${({ theme }) => theme.typographyVariants.title.lineHeight};
   `,
-  paragraph1 : css `
-      font-size:${({ theme }) => theme.typographyVariants.paragraph1.fontSize};
-      font-weight:${({ theme }) =>theme.typographyVariants.paragraph1.font_weight};
-      line-height:${({ theme }) =>theme.typographyVariants.paragraph1.lineHeight};
-  
-  `
 
+  titleXS: css`
+    font-size: ${({ theme }) => theme.typographyVariants.titleXS.fontSize};
+    font-weight: ${({ theme }) => theme.typographyVariants.titleXS.font_weight};
+    line-height: ${({ theme }) => theme.typographyVariants.titleXS.lineHeight};
+  `,
+  subTitle: css`
+    font-size: ${({ theme }) => theme.typographyVariants.subTitle.fontSize};
+    font-weight: ${({ theme }) =>
+      theme.typographyVariants.subTitle.font_weight};
+    line-height: ${({ theme }) => theme.typographyVariants.subTitle.lineHeight};
+  `,
 
-}
+  paragraph1: css`
+    font-size: ${({ theme }) => theme.typographyVariants.paragraph1.fontSize};
+    font-weight: ${({ theme }) =>
+      theme.typographyVariants.paragraph1.font_weight};
+    line-height: ${({ theme }) =>
+      theme.typographyVariants.paragraph1.lineHeight};
+  `,
+  paragraph2: css`
+    font-size: ${({ theme }) => theme.typographyVariants.paragraph2.fontSize};
+    font-weight: ${({ theme }) =>
+      theme.typographyVariants.paragraph2.font_weight};
+    line-height: ${({ theme }) =>
+      theme.typographyVariants.paragraph2.lineHeight};
+  `,
+
+  smallestException: css`
+    font-size: ${({ theme }) =>
+      theme.typographyVariants.smallestException.fontSize};
+    font-weight: ${({ theme }) =>
+      theme.typographyVariants.smallestException.font_weight};
+    line-height: ${({ theme }) =>
+      theme.typographyVariants.smallestException.lineHeight};
+  `,
+};
 
 const TextBase = styled.span`
   ${(props) => TextStyleVartiansMap[props.variant]}
