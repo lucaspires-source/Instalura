790e9657677ca8accb1242b82f459bd8dd5aede7
lucaspires-source <pires.lucas94@gmail.com>
2021-08-01 14:48:40 -0300

    estrutura basica

---
+++ src/components/foundation/layout/Grid/index.js
@@ -0,0 +1,13 @@
+import styled,{ css } from 'styled-components'
+
+export const Grid = {
+    Container: styled.div `
+    
+    `,
+    Col: styled.div `
+    
+    `,
+    Row: styled.div `
+    
+     `,
+}
