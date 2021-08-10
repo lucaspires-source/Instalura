3d2f42816b8f974fdfeb082811063ecd8e252916
lucaspires-source <pires.lucas94@gmail.com>
2021-08-10 14:18:47 -0300

    implementando animacao ao modal

--- src/components/commons/Modal/index.js
+++ src/components/commons/Modal/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
 import { ModalWrapper } from './styles';
 
 const Modal = ({ isOpen, onClose, children }) => (
@@ -12,7 +13,19 @@ const Modal = ({ isOpen, onClose, children }) => (
       }
     }}
   >
-    <div
+    <motion.div
+      variants={{
+        open: {
+          x: 0,
+        },
+        closed: {
+          x: -'-100%',
+        },
+      }}
+      animate={isOpen ? 'open' : 'closed'}
+      transition={{
+        duration: 0.5,
+      }}
       style={{
         display: 'flex',
         flex: '1',
@@ -22,7 +35,7 @@ const Modal = ({ isOpen, onClose, children }) => (
       {children({
         'data-modal-safe-area': 'true',
       })}
-    </div>
+    </motion.div>
 
   </ModalWrapper>
 );
