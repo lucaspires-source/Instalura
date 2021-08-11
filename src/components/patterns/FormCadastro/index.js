86bc5fcc68b8c62354c0f7eddacad2a98f18240a
lucaspires-source <pires.lucas94@gmail.com>
2021-08-11 15:50:48 -0300

    Aplicando  Box & Grid no Form Cadastro

--- src/components/patterns/FormCadastro/index.js
+++ src/components/patterns/FormCadastro/index.js
@@ -1,11 +1,29 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import FormContent from '../FormContent';
-// eslint-disable-next-line react/prop-types
-const FormCadastro = ({ props }) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  <div {...props}>
-    <FormContent />
-  </div>
-);
+import Box from '../../foundation/layout/Box';
+import Grid from '../../foundation/layout/Grid';
 
+const FormCadastro = ({ props }) => (
+
+  <Grid.Row marginLeft={0} marginRight={0} flex={1} justifyContent="flex-end">
+    <Grid.Col display="flex" paddingRight={{ md: '0' }} flex={1} value={{ xs: 12, md: 5, lg: 4 }}>
+      <Box
+        boxShadow="-10px 0px 24px rgba(7, 12, 14, 0.1)"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        flex={1}
+        padding={{
+          xs: '16px',
+          md: '85px',
+        }}
+        backgroundColor="white"
+        {...props}
+      >
+        <FormContent />
+      </Box>
+    </Grid.Col>
+  </Grid.Row>
+);
 export default FormCadastro;
