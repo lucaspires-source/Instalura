9e21f6c1eec1bca505c94db64ec1cd686a4a60aa
lucaspires-source <pires.lucas94@gmail.com>
2021-08-11 15:39:37 -0300

    Form Content Pronto

--- src/components/patterns/FormContent/index.js
+++ src/components/patterns/FormContent/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const FormContent = () => (
-  <form>
-    <div>
-      <input placeholder="Email" />
-    </div>
-    <div>
-      <input placeholder="usuario" />
-    </div>
-    <button type="submit">
-      Cadastrar
-    </button>
-  </form>
-);
+const FormContent = () => {
+  const [userInfo, setUserInfo] = useState({
+    usuario: '',
+    email: '',
+  });
 
+  const handleChange = (e) => {
+    const fieldName = e.target.getAttribute('name');
+
+    setUserInfo({
+      ...userInfo,
+      [fieldName]: e.target.value,
+    });
+  };
+
+  const isFormValid = userInfo.usuario.length === 0 || userInfo.email.length === 0;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <input
+          placeholder="Email"
+          name="email"
+          value={userInfo.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <input
+          placeholder="Usuario"
+          name="usuario"
+          value={userInfo.usuario}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit" disabled={isFormValid}>
+        Cadastrar
+      </button>
+    </form>
+  );
+};
 export default FormContent;
