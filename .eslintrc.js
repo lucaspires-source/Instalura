9a23bb632761c189398e0b72cd338e41a4365c19
lucaspires-source <pires.lucas94@gmail.com>
2021-08-15 19:20:38 -0300

    testando husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,6 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'import/prefer-default-export': 'off',
-    'linebreak-style': "off"
+    'linebreak-style': 'off',
   },
 };
