5242f7755c3af260358705ab9292cea752c1dc0a
lucaspires-source <pires.lucas94@gmail.com>
2021-08-30 15:26:12 -0300

    finalizando FAQScreen

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -7,3 +7,15 @@ const PageFAQ = (props) => (
 );
 
 export default PageFAQ;
+
+export async function getStaticProps() {
+  const faqCategories = await fetch('https://instalura-api.vercel.app/api/content/faq').then(async (res) => {
+    const response = await res.json();
+    return response.data;
+  });
+  return {
+    props: {
+      faqCategories,
+    },
+  };
+}
