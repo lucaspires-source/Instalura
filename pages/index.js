77a62b290393aed1e415215073e7797128ae9015
lucaspires-source <pires.lucas94@gmail.com>
2021-08-30 16:03:51 -0300

    adicionando SEO ao index

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from '../src/components/commons/Footer';
 import Main from '../src/components/commons/Main';
+import SEO from '../src/components/commons/SEO';
 import { Box } from '../src/components/foundation/layout/Box';
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       flexDirection="column"
       justifyContent="space-between"
     >
+      <SEO headTitle="Home" />
       <Main />
       <Footer />
     </Box>
