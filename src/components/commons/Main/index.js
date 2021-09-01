3697373477674cdeea885e4b4180340dc725af4c
lucaspires-source <pires.lucas94@gmail.com>
2021-09-01 08:16:30 -0300

    adicionando um provider para os themas e globalstyles

--- src/components/commons/Main/index.js
+++ src/components/commons/Main/index.js
@@ -9,73 +9,73 @@ const Main = () => {
   const websitePageContext = useContext(WebsitePageContext);
 
   return (
-      <Box
-        display="flex"
-        flexDirection="column"
-        flex="1"
+    <Box
+      display="flex"
+      flexDirection="column"
+      flex="1"
+    >
+      <Grid.Container
+        marginTop={{
+          xs: '32px',
+          md: '75px',
+        }}
       >
-        <Grid.Container
-          marginTop={{
-            xs: '32px',
-            md: '75px',
-          }}
-        >
-          <Grid.Row>
-            <Grid.Col
-              offset={{ xs: 0, md: 1 }}
-              value={{ xs: 12, md: 5 }}
-              display="flex"
-              alignItems="flex-start"
-              justifyContent="center"
-              flexDirection="column"
+        <Grid.Row>
+          <Grid.Col
+            offset={{ xs: 0, md: 1 }}
+            value={{ xs: 12, md: 5 }}
+            display="flex"
+            alignItems="flex-start"
+            justifyContent="center"
+            flexDirection="column"
+          >
+            <Text
+              variant="title"
+              tag="h1"
+              color="tertiary.main"
+              textAlign={{
+                xs: 'center',
+                md: 'left',
+              }}
             >
-              <Text
-                variant="title"
-                tag="h1"
-                color="tertiary.main"
-                textAlign={{
-                  xs: 'center',
-                  md: 'left',
-                }}
-              >
-                Compartilhe momentos e conecte-se com amigos
-              </Text>
-              <Text
-                variant="paragraph1"
-                tag="p"
-                color="tertiary.light"
-                textAlign={{
-                  xs: 'center',
-                  md: 'left',
-                }}
-              >
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industrys standard dummy text
-                ever since the 1500s.
-              </Text>
+              Compartilhe momentos e conecte-se com amigos
+            </Text>
+            <Text
+              variant="paragraph1"
+              tag="p"
+              color="tertiary.light"
+              textAlign={{
+                xs: 'center',
+                md: 'left',
+              }}
+            >
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industrys standard dummy text
+              ever since the 1500s.
+            </Text>
 
-              <Button
-                variant="primary.main"
-                margin={{
-                  xs: 'auto',
-                  md: 'initial',
-                }}
-                display="block"
-                onClick={websitePageContext.toggleModalCadastro}
-              >
-                Cadastrar
-              </Button>
-            </Grid.Col>
-            <Grid.Col value={{ xs: 12, md: 6 }}>
-              <img
-                style={{ display: 'block', margin: 'auto' }}
-                src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
-                alt="imagem ilustrativa"
-              />
-            </Grid.Col>
-          </Grid.Row>
-        </Grid.Container>
-      </Box>
+            <Button
+              variant="primary.main"
+              margin={{
+                xs: 'auto',
+                md: 'initial',
+              }}
+              display="block"
+              onClick={websitePageContext.toggleModalCadastro}
+            >
+              Cadastrar
+            </Button>
+          </Grid.Col>
+          <Grid.Col value={{ xs: 12, md: 6 }}>
+            <img
+              style={{ display: 'block', margin: 'auto' }}
+              src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
+              alt="imagem ilustrativa"
+            />
+          </Grid.Col>
+        </Grid.Row>
+      </Grid.Container>
+    </Box>
   );
 };
 
---
+++ src/components/wrappers/WebsitePage/hoc/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// eslint-disable-next-line arrow-body-style
+const websitePageHOC = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default websitePageHOC
