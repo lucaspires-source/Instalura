86ce9ef58948ed26f772042d4dc2cf072b39922a
lucaspires-source <pires.lucas94@gmail.com>
2021-09-01 10:21:49 -0300

    pequeno erro

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import WebsitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
 
+const style = {
+  display: 'flex',
+  justifyContent: 'center',
+};
 const PageSobre = () => (
-  <div>
-    Sobre
+  <div style={style}>
+    <img src="https://www.saojoaoevangelista.mg.leg.br/institucional/noticias/site-em-construcao/image" alt="paginaconstrucao" />
   </div>
 );
 
-export default PageSobre;
+export default WebsitePageHOC(PageSobre, {
+  pageWrapperProps: {
+    seoProps: { headTitle: 'Sobre' },
+  },
+});
