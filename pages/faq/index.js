65031e1f772399543661b7cc773bbe243ac991a2
lucaspires-source <pires.lucas94@gmail.com>
2021-09-01 10:07:18 -0300

    aplicando nova estrutura ao Faq

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import FAQScreen from '../../src/components/screens/FAQScreen';
+import WebsitePageHOC from '../../src/components/wrappers/WebsitePage/hoc';
 
 const PageFAQ = (props) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
   <FAQScreen {...props} />
 );
 
-export default PageFAQ;
-
+export default WebsitePageHOC(PageFAQ,{
+  pageWrapperProps: {
+    seoProps: { headTitle: 'FAQ' },
+  },
+});
 export async function getStaticProps() {
   const faqCategories = await fetch('https://instalura-api.vercel.app/api/content/faq').then(async (res) => {
     const response = await res.json();
