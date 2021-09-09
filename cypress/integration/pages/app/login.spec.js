9e245ec343025f4e25307a40be91dad812febe99
lucaspires-source <pires.lucas94@gmail.com>
2021-09-09 14:35:10 -0300

    criando page object para login

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -1,21 +1,16 @@
 /// <reference types="cypress" />
+import LoginScreenPageObject from '../../../../src/components/screens/app/LoginScreen/LoginScreen.pageObject';
 
 describe('/pages/app/login/', () => {
   // it === test que estamos fazendo
   it('preencha os campos e vá para a página /app/profile', () => {
     cy.intercept('https://instalura-api-git-master-omariosouto.vercel.app/api/login')
       .as('userLogin');
-    cy.visit('/app/login/');
 
-    // preencher o input usuario
-    // document.querySelector('input[name="usuario"]')
-    cy.get('#formCadastro input[name="usuario"]').type('omariosouto');
-
-    // preencher o input senha
-    cy.get('#formCadastro input[name="senha"]').type('senhasegura');
-
-    // clicar no botão de submit!
-    cy.get('#formCadastro button[type="submit"]').click();
+    const loginScreen = new LoginScreenPageObject();
+    loginScreen
+      .fillLoginForm({ user: 'omariosouto', password: 'senhasegura' })
+      .submitLoginForm();
 
     // o que esperamos? ir para "/app/profile/"
     cy.url().should('include', '/app/profile');
