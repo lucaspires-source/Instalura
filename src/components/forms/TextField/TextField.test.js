bc689458e4e6cb52d9a28453658aea8d97cc09c7
lucaspires-source <pires.lucas94@gmail.com>
2021-09-18 15:35:16 -0300

    testando se o campo nao esta valido

--- src/components/forms/TextField/TextField.test.js
+++ src/components/forms/TextField/TextField.test.js
@@ -12,9 +12,32 @@ describe('<TextField />', () => {
         name="nome"
       />,
 
-    );
-
+    )
     const textField = screen.getByPlaceholderText(/nome/i);
     expect(textField).toMatchSnapshot();
+  });
+
+  describe('when field is valid', () => {
+
+  });
+
+  describe('when field is invalid', () => {
+    test('displays the respective error message', () => {
+      render(
+        <TextField
+          placeholder="email"
+          value="lucas@gmail.com"
+          onChange={() => {}}
+          name="email"
+          isTouched
+          error="o Campo email tem que ser preenchido"
+        />,
+
+      );
+      const emailField = screen.getByPlaceholderText(/email/i);
+      const alert = screen.getByRole('alert');
+      expect(emailField).toHaveValue('lucas@gmail.com');
+      expect(alert).toHaveTextContent('o Campo email tem que ser preenchido');
+    });
   });
 });
