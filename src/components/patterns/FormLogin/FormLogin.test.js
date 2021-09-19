4682276d5d6560fe12f8d14f35788b7c7622ba61
lucaspires-source <pires.lucas94@gmail.com>
2021-09-19 08:03:26 -0300

    resolvendo problemas com act

--- src/components/patterns/FormLogin/FormLogin.test.js
+++ src/components/patterns/FormLogin/FormLogin.test.js
@@ -1,25 +1,30 @@
 import React from 'react';
 // import user from '@testing-library/user-event';
 import FormLogin from './index';
-import { render } from '../../../infra/test/testUtils';
+import { render, act } from '../../../infra/test/testUtils';
 
 describe('<FormLogin />', () => {
   describe('when form fields are valid', () => {
-    test('complete the submission', () => {
+    test('complete the submission', async () => {
       const onSubmit = jest.fn();
-      render(
-        <FormLogin onSubmit={onSubmit} />,
-      );
+      await act(async () => {
+        render(
+          <FormLogin onSubmit={onSubmit} />,
+        );
+      });
 
       expect(onSubmit).toHaveBeenCalledTimes(1);
     });
   });
+
   describe('when form fields are invalid', () => {
-    test('inmcomplete the submission', () => {
+    test('complete the submission', async () => {
       const onSubmit = jest.fn();
-      render(
-        <FormLogin onSubmit={onSubmit} />,
-      );
+      await act(async () => {
+        render(
+          <FormLogin onSubmit={onSubmit} />,
+        );
+      });
 
       expect(true).toBe(true);
     });
