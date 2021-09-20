21d0a0f17b1745b570f2128fb212bd177235c67d
lucaspires-source <pires.lucas94@gmail.com>
2021-09-20 10:32:02 -0300

    nova estrutura pro useForm hook

--- src/infra/hooks/forms/useForm/index.js
+++ src/infra/hooks/forms/useForm/index.js
@@ -1,5 +1,16 @@
 import { useState, useEffect } from 'react';
 
+function formatErrors(yupErrorsInner = []) {
+  return yupErrorsInner.reduce((errorObjectAcc, currentError) => {
+    const fieldName = currentError.path;
+    const errorMessage = currentError.message;
+    return {
+      ...errorObjectAcc,
+      [fieldName]: errorMessage,
+    };
+  }, {});
+}
+
 const useForm = ({ initialValues, onSubmit, validateSchema }) => {
   const [values, setValues] = useState(initialValues);
 
@@ -13,14 +24,7 @@ const useForm = ({ initialValues, onSubmit, validateSchema }) => {
       setErrors({});
       setIsFormDisabled(false);
     } catch (err) {
-      const formatedErrors = err.inner.reduce((errorObjectAcc, currentError) => {
-        const fieldName = currentError.path;
-        const errorMessage = currentError.message;
-        return {
-          ...errorObjectAcc,
-          [fieldName]: errorMessage,
-        };
-      }, {});
+      const formatedErrors = formatErrors(err.inner);
       setErrors(formatedErrors);
       setIsFormDisabled(true);
     }
