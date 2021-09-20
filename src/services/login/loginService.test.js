106d1691a5a5036ec9198b0e128c04efa748c8c9
lucaspires-source <pires.lucas94@gmail.com>
2021-09-20 09:25:03 -0300

    finalizando os testes no LoginService.js

--- src/services/login/loginService.test.js
+++ src/services/login/loginService.test.js
@@ -1,8 +1,52 @@
 import { loginService } from './loginService';
 
+const token = 'fake-token';
+async function HttpClientModule() {
+  return {
+    data: {
+      token,
+    },
+  };
+}
+async function HttpClientModuleError() {
+  return {
+    data: {},
+    err: {
+      message: 'Failed to login',
+    },
+  };
+}
+const setCookieModule = jest.fn();
+
 describe('loginService', () => {
   describe('login()', () => {
-
+    describe('when user try to login', () => {
+      describe('and succeed', () => {
+        test('store its token', async () => {
+          const loginServiceRes = await loginService.login({
+            username: 'nome',
+            password: 'senha',
+          }, setCookieModule, HttpClientModule);
+          expect(setCookieModule).toHaveBeenCalledWith(
+            null, 'APP_TOKEN', token, {
+              path: '/',
+              maxAge: 604800,
+            },
+          );
+          expect(loginServiceRes).toEqual({ token });
+        });
+      });
+      describe('and it fails', () => {
+        test('throws an error', async () => {
+          await expect(loginService.login({
+            username: 'someusername',
+            password: 'somepassword',
+          }, setCookieModule, HttpClientModuleError))
+            .rejects
+            .toThrow('Failed to login');
+        });
+      });
+    });
   });
 
   describe('logout()', () => {
