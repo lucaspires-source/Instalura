d56f1ccc55d5302729f96b5901d20aa85d5aab01
lucaspires-source <pires.lucas94@gmail.com>
2021-09-22 14:32:45 -0300

    preparando a estrutura da pagina sobre

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import WebsitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
+import { Box } from '../src/components/foundation/layout/Box';
+import { Grid } from '../src/components/foundation/layout/Grid';
+import Text from '../src/components/foundation/Text';
 
-const style = {
-  display: 'flex',
-  justifyContent: 'center',
-};
 const PageSobre = () => (
-  <div style={style}>
-    <img src="https://www.saojoaoevangelista.mg.leg.br/institucional/noticias/site-em-construcao/image" alt="paginaconstrucao" />
-  </div>
+  <Box
+    display="flex"
+    flexDirection="column"
+    flex={1}
+  >
+    <Grid.Container>
+      <Grid.Row
+        marginTop={{ xs: '32px', md: '120px' }}
+        flex="1"
+      >
+        <Grid.Col
+          value={{ xs: 12, md: 6, lg: 6 }}
+          offset={{ md: 2 }}
+          flex={1}
+        >
+          <Text
+            variant="title"
+            tag="h2"
+            color="tertiary.main"
+          >
+            Pagina Sobre
+          </Text>
+          <Box>
+            Conteudo da Pagina Sobre
+          </Box>
+        </Grid.Col>
+      </Grid.Row>
+    </Grid.Container>
+  </Box>
 );
 
 export default WebsitePageHOC(PageSobre, {
