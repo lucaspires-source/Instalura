7bf1d5ee4c5e108181a0735c60f621ea8b75a2c4
lucaspires-source <pires.lucas94@gmail.com>
2021-09-23 16:32:46 -0300

    refatoracao

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,11 +1,7 @@
-import React from 'react';
 import { GraphQLClient, gql } from 'graphql-request';
-import PropTypes from 'prop-types';
 import WebsitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
-import { Box } from '../src/components/foundation/layout/Box';
-import { Grid } from '../src/components/foundation/layout/Grid';
-import Text from '../src/components/foundation/Text';
 import { TOKEN } from '../src/infra/env/apiTokenEnv';
+import AboutScreen from '../src/components/screens/AboutScreen';
 
 export async function getStaticProps() {
   const DatoCMSURL = 'https://graphql.datocms.com/';
@@ -33,48 +29,9 @@ export async function getStaticProps() {
     },
   };
 }
-const PageSobre = ({ messages }) => {
-  console.log(messages);
-  return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      flex={1}
-    >
-      <Grid.Container>
-        <Grid.Row
-          marginTop={{ xs: '32px', md: '120px' }}
-          flex="1"
-        >
-          <Grid.Col
-            value={{ xs: 12, md: 6, lg: 6 }}
-            offset={{ md: 2 }}
-            flex={1}
-          >
-            <Text
-              variant="title"
-              tag="h2"
-              color="tertiary.main"
-            >
-              {messages.pageSobre.pageTitle}
-            </Text>
-            <Box>
-              {messages.pageSobre.pageDescription}
-            </Box>
-          </Grid.Col>
-        </Grid.Row>
-      </Grid.Container>
-    </Box>
-  );
-};
 
-export default WebsitePageHOC(PageSobre, {
+export default WebsitePageHOC(AboutScreen, {
   pageWrapperProps: {
     seoProps: { headTitle: 'Sobre' },
   },
 });
-
-PageSobre.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
-  messages: PropTypes.object.isRequired,
-};
