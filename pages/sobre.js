ee7f1c40d3422ab352a51fb68b48927654573014
lucaspires-source <pires.lucas94@gmail.com>
2021-09-23 16:14:48 -0300

    query feita

--- pages/sobre.js
+++ pages/sobre.js
@@ -4,38 +4,68 @@ import WebsitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
 import { Box } from '../src/components/foundation/layout/Box';
 import { Grid } from '../src/components/foundation/layout/Grid';
 import Text from '../src/components/foundation/Text';
+import { TOKEN } from '../src/infra/env/apiTokenEnv';
 
-const PageSobre = () => (
-  <Box
-    display="flex"
-    flexDirection="column"
-    flex={1}
-  >
-    <Grid.Container>
-      <Grid.Row
-        marginTop={{ xs: '32px', md: '120px' }}
-        flex="1"
-      >
-        <Grid.Col
-          value={{ xs: 12, md: 6, lg: 6 }}
-          offset={{ md: 2 }}
-          flex={1}
+export async function getStaticProps() {
+  const DatoCMSURL = 'https://graphql.datocms.com/';
+
+  const client = new GraphQLClient(DatoCMSURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+
+    },
+  });
+  const query = gql`
+    query {
+      pageSobre {
+        pageTitle
+        pageDescription
+      }
+    }
+  `;
+
+  const messages = await client.request(query);
+
+  return {
+    props: {
+      messages,
+    },
+  };
+}
+const PageSobre = ({ messages }) => {
+  console.log(messages);
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      flex={1}
+    >
+      <Grid.Container>
+        <Grid.Row
+          marginTop={{ xs: '32px', md: '120px' }}
+          flex="1"
         >
-          <Text
-            variant="title"
-            tag="h2"
-            color="tertiary.main"
+          <Grid.Col
+            value={{ xs: 12, md: 6, lg: 6 }}
+            offset={{ md: 2 }}
+            flex={1}
           >
-            Pagina Sobre
-          </Text>
-          <Box>
-            Conteudo da Pagina Sobre
-          </Box>
-        </Grid.Col>
-      </Grid.Row>
-    </Grid.Container>
-  </Box>
-);
+            <Text
+              variant="title"
+              tag="h2"
+              color="tertiary.main"
+            >
+              {messages.pageTitle}
+            </Text>
+            <Box>
+              {messages.pageDescription}
+            </Box>
+          </Grid.Col>
+        </Grid.Row>
+      </Grid.Container>
+    </Box>
+  );
+};
 
 export default WebsitePageHOC(PageSobre, {
   pageWrapperProps: {
