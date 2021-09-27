04abc7f0b09c2633c399fa66ab8700b18a6c4357
lucaspires-source <pires.lucas94@gmail.com>
2021-09-27 14:52:29 -0300

    BUGS CORRIGIDOS

--- pages/api/exit-preview.js
+++ pages/api/exit-preview.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   res.clearPreviewData({});
-  const key = process.env.KEY;
+  const key = 'BIRDMAN';
   if (req.query.key !== key) {
     return res.status(401).json({ message: 'invalid key to enable preview' });
   }
