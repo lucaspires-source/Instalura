0efc6696f86b5ae3942b1dc97fcddd4248ee4193
lucaspires-source <pires.lucas94@gmail.com>
2021-09-27 14:49:42 -0300

    adicionano protecao as Lambdas

--- pages/api/exit-preview.js
+++ pages/api/exit-preview.js
@@ -1,6 +1,9 @@
 export default function handler(req, res) {
   res.clearPreviewData({});
-
+  const key = process.env.KEY;
+  if (req.query.key !== key) {
+    return res.status(401).json({ message: 'invalid key to enable preview' });
+  }
   res.writeHead(307, { location: '/' });
 
   return res.end();
